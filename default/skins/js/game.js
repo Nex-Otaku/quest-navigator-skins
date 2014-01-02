2d5370fb05c469d4a8ce5a5b592f42edaac3c1e4
Nex Otaku <nex@otaku.ru>
2014-01-02 18:45:33 +0400

    поддержка строки ввода в стандартном скине

--- default/skins/js/game.js
+++ default/skins/js/game.js
@@ -1,2 +1,7 @@
 /* Функции скина для игры */
 
+function qspSkinOnUpdateSkin() {
+	// Когда строка ввода видна, элемент BODY получает класс "input-visible".
+	// Это позволяет нам определить правила CSS, зависящие от видимости строки ввода.
+	$(document.body).toggleClass('input-visible', $("#qsp-input-line").is(":visible"));
+}
