e1fbe0ad1648905dee8fdf0290e118a38bbf19ce
Nex Otaku <nex@otaku.ru>
2014-10-19 15:30:25 +0400

    обновил яваскрипт классического шаблона

--- classic/skins/js/game.js
+++ classic/skins/js/game.js
@@ -3,10 +3,9 @@
 function qspSkinOnUpdateSkin() {
 	// Когда строка ввода видна, элемент BODY получает класс "input-visible".
 	// Это позволяет нам определить правила CSS, зависящие от видимости строки ввода.
-	$(document.body).toggleClass('input-visible', $("#qsp-input-line").is(":visible"));
+	$(document.body).toggleClass('input-visible', qspGameSkin.showInput == 1);
 	// Аналогично для остальных окошек
-	var actsVisible = qspGameSkin.showActs == 1;
-	$(document.body).toggleClass('acts-visible', actsVisible);
-	$(document.body).toggleClass('objs-visible', $("#qsp-wrapper-objs").is(":visible"));
-	$(document.body).toggleClass('vars-visible', $("#qsp-wrapper-vars").is(":visible"));
+	$(document.body).toggleClass('acts-visible', qspGameSkin.showActs == 1);
+	$(document.body).toggleClass('objs-visible', qspGameSkin.showObjs == 1);
+	$(document.body).toggleClass('vars-visible', qspGameSkin.showVars == 1);
 }
